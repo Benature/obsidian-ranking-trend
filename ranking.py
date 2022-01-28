c92fe54f280d70856c4ae0dfc2733b27c1846578
Benature <wbenature@163.com>
2022-01-28 12:01:05 +0800

    fix blank dict

--- ranking.py
+++ ranking.py
@@ -1,26 +1,28 @@
+from datetime import datetime
 import requests
 import json
 
-response = requests.get('https://raw.githubusercontent.com/obsidianmd/obsidian-releases/master/community-plugin-stats.json')
+response = requests.get(
+    'https://raw.githubusercontent.com/obsidianmd/obsidian-releases/master/community-plugin-stats.json')
 
 rank_dict = json.loads(response.text)
 
 
 with open('data.json', 'r') as f:
-     data = json.load(f)
+    data = json.load(f)
 
-from datetime import datetime
 
 tstamp = datetime.strftime(datetime.today(), '%Y-%m-%d_%H:%M')
 
 today_data = {}
 for k, v in rank_dict.items():
-    today_data[k] = rank_dict[k]['downloads']
-    
+    if rank_dict[k]:
+        today_data[k] = rank_dict[k]['downloads']
+
 today_data = {
     "date": tstamp,
     "data": today_data
-    }
+}
 data.append(today_data)
 
 with open('data.json', 'w') as f:
